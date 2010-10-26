8082b2f9947b4c85a72ca4c49a4195ced0eee253
Johannes Loher <ghost91@gmx.net>
2010-10-26 16:50:59 +0200

    Removed unused error handling code

--- src/console.cpp
+++ src/console.cpp
@@ -7,30 +7,6 @@ void CConsole::Print(const char *pFmt, ...)
 	std::vprintf(pFmt, Args);
 	va_end(Args);
 }
-
-void CConsole::PrintError(Error Error)
-{
-	switch(Error)
-	{
-		case SendError:
-		Print("Failed to send data, maybe your opponent has quit the connection? Quitting the game!\n\n");
-		break;
-
-		case RecvError:
-		Print("Failed to recieve data, quitting the game!\n\n");
-		break;
-		
-		case OpponentQuit:
-		Print("Your opponent quit the connection!\n\n");
-		break;
-
-		case StartupError:
-		Print("Failed to startup. Abborting the game!\n\n");
-		case NoError:
-		break;
-	}
-	return;
-}
 	
 void CConsole::GetInteger(int *pRet)
 {
--- src/console.h
+++ src/console.h
@@ -6,20 +6,10 @@
 #include <iostream>
 #include <limits>
 
-enum Error
-{
-	SendError,
-	RecvError,
-	OpponentQuit,
-	StartupError,
-	NoError
-} ;
-
 class CConsole
 {
 	public:
 	static void Print(const char *pFmt, ...);
-	static void PrintError(Error Error);
 
 	static void GetInteger(int *pRet);
 	static void GetUnsigned(unsigned *pRet);
